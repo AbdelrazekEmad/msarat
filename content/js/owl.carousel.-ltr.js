522f1d28795acd359ac99d2c6829d9c638ee7609
Abdelrazek Emad <AbdelrazekEmad123@gmail.com>
2023-02-01 13:15:44 +0200

    testimonials En & Ar

--- content/js/owl.carousel.-ltr.js
+++ content/js/owl.carousel.-ltr.js
@@ -13,3 +13,25 @@ $(".owl-carousel-hero").owlCarousel({
     },
   },
 });
+
+$(".owl-carousel-testimonials").owlCarousel({
+  loop: true,
+  margin: 15,
+  nav: false,
+  dots: false,
+  autoplay: true,
+  animateOut: "fadeOut",
+  animateIn: "fadeIn",
+  autoplaySpeed: 6000,
+  responsive: {
+    0: {
+      items: 1,
+    },
+    600: {
+      items: 2,
+    },
+    1000: {
+      items: 3,
+    },
+  },
+});
--- content/js/owl.carousel.-rtl.js
+++ content/js/owl.carousel.-rtl.js
@@ -14,3 +14,26 @@ $(".owl-carousel-hero").owlCarousel({
     },
   },
 });
+
+$(".owl-carousel-testimonials").owlCarousel({
+  loop: true,
+  margin: 15,
+  nav: false,
+  dots: false,
+  autoplay: true,
+  animateOut: "fadeOut",
+  animateIn: "fadeIn",
+  autoplaySpeed: 6000,
+  rtl: true,
+  responsive: {
+    0: {
+      items: 1,
+    },
+    600: {
+      items: 2,
+    },
+    1000: {
+      items: 3,
+    },
+  },
+});
